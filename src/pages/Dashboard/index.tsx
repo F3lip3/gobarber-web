c0cfc1c085de9629897b3a6be6a2a369caeeca47
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2020-09-30 12:43:18 -0300

    feat: finished dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
 import { FiPower, FiClock } from 'react-icons/fi';
+import { isToday, format, isAfter, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import 'react-day-picker/lib/style.css';
+
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 import {
   Container,
   Header,
@@ -16,9 +22,118 @@ import {
   Calendar
 } from './styles';
 
+interface IMonthAvailabilityItem {
+  day: number;
+  available: boolean;
+}
+
+interface IAppointment {
+  id: string;
+  date: string;
+  formattedHour: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
+
+  const [appointments, setAppointments] = useState<IAppointment[]>([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [monthAvailability, setMonthAvailability] = useState<
+    IMonthAvailabilityItem[]
+  >([]);
+
+  const disabledDays = useMemo(() => {
+    return monthAvailability
+      .filter(monthDay => !monthDay.available)
+      .map(
+        monthDay =>
+          new Date(
+            currentMonth.getFullYear(),
+            currentMonth.getMonth(),
+            monthDay.day
+          )
+      );
+  }, [currentMonth, monthAvailability]);
+
+  const nextAppointment = useMemo(() => {
+    return appointments.find(appointment =>
+      isAfter(parseISO(appointment.date), new Date())
+    );
+  }, [appointments]);
+
+  const morningAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() < 12;
+    });
+  }, [appointments]);
+
+  const afternoonAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() >= 12;
+    });
+  }, [appointments]);
+
+  const selectedDateAsText = useMemo(() => {
+    return format(selectedDate, "'Dia' dd 'de' MMMM", {
+      locale: ptBR
+    });
+  }, [selectedDate]);
+
+  const selectedWeekDay = useMemo(() => {
+    return format(selectedDate, 'cccc', {
+      locale: ptBR
+    });
+  }, [selectedDate]);
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available && !modifiers.disabled) {
+      setSelectedDate(day);
+    }
+  }, []);
+
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month);
+  }, []);
+
+  useEffect(() => {
+    api
+      .get<IMonthAvailabilityItem[]>(
+        `providers/${user.id}/month-availability`,
+        {
+          params: {
+            year: currentMonth.getFullYear(),
+            month: currentMonth.getMonth() + 1
+          }
+        }
+      )
+      .then(response => {
+        setMonthAvailability(response.data);
+      });
+  }, [currentMonth, user.id]);
+
+  useEffect(() => {
+    api
+      .get<IAppointment[]>('appointments/me', {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate()
+        }
+      })
+      .then(response => {
+        setAppointments(
+          response.data.map(appointment => ({
+            ...appointment,
+            formattedHour: format(parseISO(appointment.date), 'HH:mm')
+          }))
+        );
+      });
+  }, [selectedDate]);
 
   return (
     <Container>
@@ -39,107 +154,100 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
-
       <Content>
         <Schedule>
           <h1>Horários Agendados</h1>
           <p>
-            <span>Hoje</span>
-            <span>Dia 06</span>
-            <span>Segunda-feira</span>
+            {isToday(selectedDate) && <span>Hoje</span>}
+            <span>{selectedDateAsText}</span>
+            <span>{selectedWeekDay}</span>
           </p>
-
-          <NextAppointment>
-            <strong>Atendimento a seguir</strong>
-            <div>
-              <img
-                src="https://avatars3.githubusercontent.com/u/2260686?s=460&u=db56ae499e020629de440282c0d9b85dd4f09213&v=4"
-                alt="Felipe Humberto Teixeira"
-              />
-              <strong>Felipe Humberto Teixeira</strong>
-              <span>
-                <FiClock />
-                08:00
-              </span>
-            </div>
-          </NextAppointment>
-
+          {isToday(selectedDate) && nextAppointment && (
+            <NextAppointment>
+              <strong>Agendamento a seguir</strong>
+              <div>
+                <img
+                  src={nextAppointment.user.avatar_url}
+                  alt={nextAppointment.user.name}
+                />
+                <strong>{nextAppointment.user.name}</strong>
+                <span>
+                  <FiClock />
+                  {nextAppointment.formattedHour}
+                </span>
+              </div>
+            </NextAppointment>
+          )}
           <Section>
             <strong>Manhã</strong>
-
-            <Appointment>
-              <span>
-                <FiClock />
-                08:00
-              </span>
-              <div>
-                <img
-                  src="https://avatars3.githubusercontent.com/u/2260686?s=460&u=db56ae499e020629de440282c0d9b85dd4f09213&v=4"
-                  alt="Felipe Humberto Teixeira"
-                />
-                <strong>Felipe Humberto Teixeira</strong>
-              </div>
-            </Appointment>
-            <Appointment>
-              <span>
-                <FiClock />
-                09:00
-              </span>
-              <div>
-                <img
-                  src="https://avatars3.githubusercontent.com/u/2260686?s=460&u=db56ae499e020629de440282c0d9b85dd4f09213&v=4"
-                  alt="Felipe Humberto Teixeira"
-                />
-                <strong>Felipe Humberto Teixeira</strong>
-              </div>
-            </Appointment>
+            {!morningAppointments.length && (
+              <p>Nenhum agendamento para manhã</p>
+            )}
+            {morningAppointments.map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {appointment.formattedHour}
+                </span>
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
-
           <Section>
             <strong>Tarde</strong>
-
-            <Appointment>
-              <span>
-                <FiClock />
-                14:00
-              </span>
-              <div>
-                <img
-                  src="https://avatars3.githubusercontent.com/u/2260686?s=460&u=db56ae499e020629de440282c0d9b85dd4f09213&v=4"
-                  alt="Felipe Humberto Teixeira"
-                />
-                <strong>Felipe Humberto Teixeira</strong>
-              </div>
-            </Appointment>
-            <Appointment>
-              <span>
-                <FiClock />
-                15:00
-              </span>
-              <div>
-                <img
-                  src="https://avatars3.githubusercontent.com/u/2260686?s=460&u=db56ae499e020629de440282c0d9b85dd4f09213&v=4"
-                  alt="Felipe Humberto Teixeira"
-                />
-                <strong>Felipe Humberto Teixeira</strong>
-              </div>
-            </Appointment>
-            <Appointment>
-              <span>
-                <FiClock />
-                16:00
-              </span>
-              <div>
-                <img
-                  src="https://avatars3.githubusercontent.com/u/2260686?s=460&u=db56ae499e020629de440282c0d9b85dd4f09213&v=4"
-                  alt="Felipe Humberto Teixeira"
-                />
-                <strong>Felipe Humberto Teixeira</strong>
-              </div>
-            </Appointment>
+            {!afternoonAppointments.length && (
+              <p>Nenhum agendamento para tarde</p>
+            )}
+            {afternoonAppointments.map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {appointment.formattedHour}
+                </span>
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
         </Schedule>
-        <Calendar />
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disabledDays]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] }
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            onMonthChange={handleMonthChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro'
+            ]}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
